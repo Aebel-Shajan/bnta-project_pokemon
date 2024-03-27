9edb44aea35d09a3c2d773a5b329037a7501d30a
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-27 13:59:07 +0000

    added pokemonform

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,5 +1,29 @@
-const PokemonForm = () => {
-    return ( <></>);
+import {useState} from "react";
+
+const PokemonForm = (event) => {
+   const [search, setSearch] = useState("");
+    
+    const handleChange = (event) => {
+        setSearch(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        
+    }
+
+
+    return ( 
+    <form onSubmit={handleSubmit}>
+        <input
+        type = "text"
+        placeholder="Seach Pokemon.."
+        value={search}
+        onChange={handleChange}
+        />
+        <input type="submit" value="Search" />
+    </form>
+    
+    );
 }
  
 export default PokemonForm;
